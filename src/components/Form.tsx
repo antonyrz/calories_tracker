35a9e7f88563a08d46aebc7fa69f2b3551ccb428
Antonyhz <hernandezantony333@gmail.com>
2025-07-22 16:21:44 -0400

    onChange al form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,15 +1,29 @@
+import { act, useState } from "react";
 import { categories } from "../data/categories";
 
 export default function Form() {
+
+    const [activity, setActivity] = useState({
+        category: 1,
+        name: '',
+        calories: 0
+    });
+
+    const handleChange = () => {
+        
+    }
+
   return (
     <>
         <form action="" className="space-y-5 bg-white shadow p-10 rounded-lg">
             <div className="grid gap-3">
                 <label htmlFor="category" className="font-bold">Categoría:</label>
                 <select 
+                value={activity.category}
+                onChange={handleChange}
                 name="" id="category"
                 className="border border-slate-300 p-2 rounded-lg w-full bg-white">
-                        <option disabled selected value="">-- Seleccione --</option>
+                        <option disabled defaultValue={} value="">-- Seleccione --</option>
                     {categories.map((category) => (
                         <option 
                             key={category.id}
@@ -23,6 +37,8 @@ export default function Form() {
             <div className="grid gap-3">
                 <label htmlFor="activity" className="font-bold">Actividad:</label>
                 <input 
+                onChange={handleChange}
+                value={activity.name}
                 className="border border-slate-300 p-2 rounded-lg"
                 type="text"
                 id="activity"
@@ -33,6 +49,8 @@ export default function Form() {
              <div className="grid gap-3">
                 <label htmlFor="calories" className="font-bold">Calorías:</label>
                 <input 
+                onChange={handleChange}
+                value={activity.calories}
                 className="border border-slate-300 p-2 rounded-lg"
                 type="number"
                 id="calories"
